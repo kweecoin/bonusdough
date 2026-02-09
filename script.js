2ff01d6eb476c0660ba3c9fab2cd55d833dbbe25
kweecoin <jacobidee9@gmail.com>
2026-02-09 05:42:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+https://www.bitunix.com/register?inviteCode=bhjwaz
 document.addEventListener("DOMContentLoaded", () => {
 
   const OFFERS = [
@@ -6,49 +6,56 @@ document.addEventListener("DOMContentLoaded", () => {
       brand: "STAKE.US",
       payout: "$25",
       country: "USA",
-      requirement: "Sign up",
+      requirement: "Sign-up",
       link: "https://stake.us/?c=25dollars4free"
+    },
+    {
+      brand: "Bitunix Pro - Crytpo Trading & Wallet",
+      payout: "18% Of All Trades & Buys",
+      country: "USA",
+      requirement: "Sign up + referral.",
+      link: "https://www.bitunix.com/register?inviteCode=bhjwaz"
     },
     {
       brand: "AttaPoll",
       payout: "OfferWall",
       country: "USA",
-      requirement: "Sign up + referral. Non-KYC offerwall & surveys.",
+      requirement: "Sign-up + referral. Non-KYC offerwall & surveys.",
       link: "https://attapoll.app/join/axqlj"
     },
     {
       brand: "Spritz.Finance (Crypto)",
       payout: "$35",
       country: "USA",
-      requirement: "Sign up only and receive $35 in crypto.",
+      requirement: "Sign-up only and receive $35 in crypto.",
       link: "https://spritz.finance"
     },
     {
       brand: "Earnlab.com",
       payout: "Earn $ from offers",
       country: "USA",
-      requirement: "Sign up & Earn",
+      requirement: "Sign-up & Earn",
       link: "https://earnlab.com/r/787"
     },
     {
       brand: "Current Bank",
       payout: "$100",
       country: "USA",
-      requirement: "Sign up + $200 deposit (required).",
+      requirement: "Sign-up + $200 deposit (required).",
       link: "https://current.com/get-started/?creator_code=JACOBID311&impression_id=f40dd529-c101-4e46-9af2-ae095a91ab9f"
     },
     {
       brand: "Splash Sports - Sports betting app",
       payout: "$10",
       country: "USA",
-      requirement: "Sign up.",
+      requirement: "Sign-up.",
       link: "https://link.splashsports.com/6OOI/c36h0gvs"
     },
     {
       brand: "Stash",
       payout: "$5",
       country: "USA",
-      requirement: "Sign up & Deposit $5 and get $5.",
+      requirement: "Sign-up & Deposit $5 and get $5.",
       link: "https://publishers.revenueuniverse.com/click.php?affiliate=8189&campaign=42991&sid=SFB_1090___&app=1090&sid2=f27d3a5f-1271-4b31-9082-39a6b"
     },
     {
